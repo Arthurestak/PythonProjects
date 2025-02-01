890f6cf205407ebcc72893790f3a32b286c86a4e
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2025-02-01 20:27:26 -0300

    Update ValidadorCPF.py

--- ValidadorCPF.py
+++ ValidadorCPF.py
@@ -1,17 +1,17 @@
 import os 
 
 while True:
-    decrescente = 10
-    cpf_entrada = input('Digite os 9 primeiros dígitos do CPF (Não inclua ponto.): ')
-    confirma = input('Digite "Enter" se digitou corretamente e "N" se não!').lower()
+    decrescenteUm = 10
+    cpf_nove_digitos = input('Digite os 9 primeiros dígitos do CPF (Não inclua ponto.): ')
+    confirma = input('Digite "Enter" se digitou corretamente e "N" se não: ').lower()
     os.system('cls')
     if confirma.startswith('n'):
         continue
-    if len(cpf_entrada) == 9:
+    if len(cpf_nove_digitos) == 9:
 
         cpf_multi = []
 
-        for [i] in cpf_entrada:
+        for [i] in cpf_nove_digitos:
 
             try:
                 iNum = int(i)
@@ -20,11 +20,11 @@ while True:
                 print('Digite um valor válido!')
                 continue
 
-            multi = decrescente * iNum
+            multi = decrescenteUm * iNum
 
             cpf_multi.append(multi)
 
-            decrescente -= 1
+            decrescenteUm -= 1
 
         a,b,c,d,e,f,g,h,i = cpf_multi
 
@@ -37,31 +37,59 @@ while True:
         if resto >= 10:
             print('O próximo número do CPF é: 0')
 
-            reiniciar = input('Digite [S] se quiser realizar outra operação, e [N] para sair: ').lower()
+            reiniciar = input('Digite [S] se deseja refazer operação, e ENTER para prosseguir: ').lower()
 
             if reiniciar.startswith('s'):
                 os.system('cls')
                 continue
-            elif reiniciar.startswith('n'):
+            else:
                 os.system('cls')
                 break
-            else:
-                print('Digite uma das duas opções [S], [N]!!')
-                continue
         else:
             print('O próximo número do CPF é: ',resto)
             
-            reiniciar = input('Digite [S] se quiser realizar outra operação, e [N] para sair: ').lower()
+            reiniciar = input('Digite [S] se deseja refazer operação, e ENTER para prosseguir: ').lower()
 
             if reiniciar.startswith('s'):
                 os.system('cls')
                 continue
-            elif reiniciar.startswith('n'):
+            else:
                 os.system('cls')
                 break
-            else:
-                print('Digite uma das duas opções [S], [N]!!')
-                continue
     else:
         print('Digite um valor válido, apenas os 9 primeiros números!\n')
         continue
+
+decrescenteDois = 11
+
+cpf_dez_digitos = input('Deseja que eu descubra o último número também? [S], [N] ').lower()
+os.system('cls')
+if cpf_dez_digitos.startswith('s'):
+    cpf_novo_lista = []
+    for [i] in cpf_nove_digitos:
+        cpf_novo_lista.append(i)
+    cpf_novo_lista.append(resto) if resto <= 9 else cpf_novo_lista.append(0)
+    
+    cpf_multi2 = []
+
+    cpf_completo = []
+
+    for i in cpf_novo_lista:
+        iNum = int(i)
+        multi3 = decrescenteDois * iNum
+        cpf_multi2.append(multi3)
+        decrescenteDois -= 1 
+    j,k,l,m,n,o,p,q,r,s = cpf_multi2
+
+    soma2 = j+k+l+m+n+o+p+q+r+s
+
+    multi4 = soma2 * 10    
+
+    resto2 = multi4 % 11
+
+    if resto2 <= 9:
+        cpf_novo_lista.append(resto2)
+        print('O seu CPF completo é: ',"".join(map(str,cpf_novo_lista)))
+else:
+    os.system('cls')
+    ...
